a87771f558dcdfc24899b0e81e74151854db4bdb
Eva Ortiz <evamariaob76.com>
2022-06-17 19:13:32 +0200

    cambios responsive4

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -2,7 +2,6 @@ import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../auth/AuthContext';
 import Swal from 'sweetalert2';
-import {  Button } from '@mui/material';
 
 
 
@@ -63,79 +62,76 @@ const todoOK=()=>{
 }
 
     return (
-   
-<>
-<div className="mbsc-row container" >
-        <div className="  avatarRegistro ocultar-div ">            
+    <div className='container'>
+                <div className="img ">            
+
+        <div className="row login100-form avatarRegistro ">            
         </div>
-    <div className="mbsc-col-12 mbsc-col-md-6 mbsc-col-lg-3">
+
+        </div>
+              <div className='form-group'>
+        <form className="login100-form validate-form flex-sb flex-w"
+               onSubmit={onSubmit}>
             <span className="login100-form-title mb-3">
                  Ingreso
             </span>
-    </div>
-        <form className="login100-form validate-form flex-sb flex-w"
-               onSubmit={onSubmit}>
-    <div className="mbsc-col-12 mbsc-col-md-6 mbsc-col-lg-3 input" >
-        <input 
-            label="Password" 
-            className=" form-control " 
-            type="email" 
-            name="email" 
-            placeholder="Email"
-             value={form.email}
-            onChange={onChange}/>
-    </div>
 
-<div className="mbsc-row input">
-    <div className="mbsc-col-12 mbsc-col-md-6 mbsc-col-lg-3 ">
-        <input 
-            className=" form-control input" 
-            type="password" 
-            name="password" 
-            placeholder="Password"
-            value={form.password}
-            onChange={onChange} 
-            autoComplete="on"/>
-    </div>
+            <div className="wrap-input100 validate-input mb-3">
+                <input 
+                    className="input100" 
+                    type="email" 
+                    name="email" 
+                    placeholder="Email"
+                    value={form.email}
+                    onChange={onChange} />
+                <span className="focus-input100"></span>
+            </div>
 
-</div>
-<div className="mbsc-row input">
-    <div className="mbsc-col-12 mbsc-col-md-6 mbsc-col-lg-3 " onClick={()=>toogleCheck()}>
 
-        <input 
-            className="input-checkbox100" 
-            id="ckb1" 
-            type="checkbox" 
-            name="rememberMe"
-            checked={form.rememberMe}
-            readOnly />
-                       <label className="label-checkbox100 responsive">
+            <div className="wrap-input100 validate-input mb-3">
+                <input
+                     className="input100" 
+                     type="password" 
+                     name="password" 
+                     placeholder="Password"
+                     value={form.password}
+                     onChange={onChange} />
+                <span className="focus-input100"></span>
+            </div>
+
+            <div className=" mb-3">
+                <div className="col"
+                     onClick={()=>toogleCheck()}>
+                    <input 
+                        className="input-checkbox100" 
+                        id="ckb1" 
+                        type="checkbox" 
+                        name="rememberMe"
+                        checked={form.rememberMe}
+                        readOnly
+
+                         />
+                    <label className="label-checkbox100">
                         Recordarme
-                 </label>
-    </div>
+                    </label>
+                </div>
 
-</div>
-<div className="mbsc-row">
-    <div className="mbsc-col-12 mbsc-col-md-12 mbsc-col-lg-3">
-        <div className="mbsc-button-group-block">
-     
-              <div className="col text-right responsive">
+                <div className="col text-right">
                     <Link to="/auth/register" className="txt1">
                         Nueva cuenta?
                     </Link>
-                </div>        </div>
-    </div>
-    <div className="mbsc-col-12 mbsc-col-md-12 mbsc-col-lg-3">
-        <div className="mbsc-button-group-block">
-            <Button type="submit" className="login100-form-btn responsive-ingresar" disabled={!todoOK()}>
-                    Ingresar
-                </Button>
-         </div>
-    </div>
-</div></form>
-</div>
+                </div>
+            </div>
 
-</>
+            <div className="container-login100-form-btn m-t-17">
+                <button type="submit" className="login100-form-btn" disabled={!todoOK()}>
+                    Ingresar
+                </button>
+            </div>
+
+        </form>
+                </div>
+                            </div>
 
     )
 }
--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -6,7 +6,6 @@ import Swal from 'sweetalert2';
 import Fab from '@mui/material/Fab';
 
 import { fileUpload } from '../helpers/fileUpload';
-import {  Button } from '@mui/material';
 import AddIcon from '@material-ui/icons/Add';
 
 
@@ -52,91 +51,79 @@ const onSubmit= async (e)=>{
 }
 
 
-
     return (
-   
-    <>
-    <div className="mbsc-row container" >
-        <div className="  avatarRegistro ocultar-div ">            
+    <div className='container'>
+                <div className="img ">            
+
+        <div className="row login100-form avatarRegistro ">            
         </div>
-    <div className="mbsc-col-12 mbsc-col-md-6 mbsc-col-lg-3">
+
+        </div>
+              <div className='form-group'>
+        <form className="login100-form "
+               onSubmit={onSubmit}>  
+                <div className='contenedor-registro'>             
             <span className="login100-form-title mb-3">
-                 REGISTRO
+                 Registro
             </span>
-    </div>
-        <form className="login100-form validate-form flex-sb flex-w"
-               onSubmit={onSubmit}>
-        <div className="mbsc-col-12 mbsc-col-md-6 mbsc-col-lg-3 input" >
-        <input 
-            label="nombre" 
-            className=" form-control " 
-            type="nombre" 
-            name="nombre" 
-            placeholder="nombre"
-             value={form.nombre}
-            onChange={onChange}/>
-    </div>
-    <div className="mbsc-col-12 mbsc-col-md-6 mbsc-col-lg-3 input" >
-        <input 
-            label="Password" 
-            className=" form-control " 
-            type="email" 
-            name="email" 
-            placeholder="Email"
-             value={form.email}
-            onChange={onChange}/>
-    </div>
 
-<div className="mbsc-row input">
-    <div className="mbsc-col-12 mbsc-col-md-6 mbsc-col-lg-3 ">
-        <input 
-            className=" form-control input" 
-            type="password" 
-            name="password" 
-            placeholder="Password"
-            value={form.password}
-            onChange={onChange} />
-    </div>
+            <div className="wrap-input100 validate-input mb-3">
+                <input className="input100 inputstl" type="text" name="nombre" placeholder="Nombre" onChange={onChange} value={form.nombre}/>
+                <span className="focus-input100"></span>
+            </div>
 
-</div>
-<div className="mbsc-row input">
-    <div className="mbsc-col-12 mbsc-col-md-6 mbsc-col-lg-3 ">
-        <input 
-            type="file"
-            multiple
-            ref={ fileInputRef }
-            onChange={ onFileInputChange }
-            style={{ display: 'none' }}
-            name="img"
-            />
-    </div>
+            
+            <div className="wrap-input100 validate-input mb-3">
+                <input className="input100" type="email" name="email" placeholder="Email"onChange={onChange} value={form.email}/>
+                <span className="focus-input100"></span>
+            </div>
+            
+            
+            <div className="wrap-input100 validate-input mb-3">
+                <input className="input100" type="password" name="password" onChange={onChange} placeholder="Password"value={form.password}/>
+                <span className="focus-input100"></span>
+            </div>
+    
+             <input 
+                    type="file"
+                    multiple
+                    ref={ fileInputRef }
+                    onChange={ onFileInputChange }
+                    style={{ display: 'none' }}
+                    name="img"
+                />
+                {/*<IconButton
+                    color="primary"
+                    onClick={ () => fileInputRef.current.click() }
+                > 
+                    <UploadOutlined />
+                </IconButton><span>Subir avatar</span>*/}
+                <div>
 
-</div>
+
+                </div>
+        <div className="col " >
+
             <Fab size="small" style={{  margin: 20 }}color="secondary" aria-label="add" onClick={ () => fileInputRef.current.click() } >
                 <AddIcon />
             </Fab><span >Subir avatar</span>
-<div className="mbsc-row">
-    <div className="mbsc-col-12 mbsc-col-md-12 mbsc-col-lg-3">
-        <div className="mbsc-button-group-block">
-      
-              <div className="col text-right responsive">
-              <Link to="/" className="txt1">
+            <div className='col text-right' style={{marginBottom:'3%'}}>
+                <Link to="/" className="txt1">
                             Tienes cuenta?
                 </Link>
-                </div>        </div>
-    </div>
-    <div className="mbsc-col-12 mbsc-col-md-12 mbsc-col-lg-3">
-        <div className="mbsc-button-group-block">
-            <Button type="submit" className="login100-form-btn responsive-ingresar" >
-                                            Crear cuenta
+             </div>
+                         </div>
 
-                </Button>
-         </div>
-    </div>
-</div></form>
-</div>
+            <div className="container-login100-form-btn m-t-17">
+                    <button type="submit" className="login100-form-btn">
+                        Crear cuenta
+                    </button>
+            </div>
+            
+        </div>
 
-    
-    </>
+        </form>
+        </div>
+    </div>
     )
 }
